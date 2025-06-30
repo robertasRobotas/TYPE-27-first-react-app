a52d648180714bd39370a725e7ec972c049b5773
Robertas Ankudovicius <r.ankudovicius@gmail.com>
2025-06-30 09:16:49 +0300

    added fetch

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -2,49 +2,15 @@ import styles from "./styles.module.css";
 import Article from "../Article/Article";
 import { useState } from "react";
 
-const Main = ({ setLogoText }) => {
-  const [articles, setArticles] = useState([
-    {
-      id: 1,
-      title: "Lietuviai skrenda atostogom i uzsieni",
-      imgUrl:
-        "https://tv3.lt/Uploads/UArticles/leadPhotos/c9/14/0e/f8/c9140ef85d741c466ed2d095fa0c2be0.jpg",
-    },
-    {
-      id: 2,
-      title:
-        "„Fenerbahče“ iki Turkijos lygos titulo atvedęs Jasikevičius: tai yra istorinis sezonas",
-      imgUrl:
-        "https://images.eurohoops.net/2025/06/d38046c2-fenerbahce-beko-950x500.jpg",
-    },
-    {
-      id: 3,
-      title: "Skelbiami dar keturių egzaminų pirmų dalių rezultatai",
-      imgUrl:
-        "https://s1.15min.lt/images/photos/2014/06/10/original/antradieni-salies-abiturientai-laiko-valstybini-istorijos-brandos-egzamina-5396a617c023d.jpg",
-    },
-    {
-      id: 4,
-      title: "Skelbiami dar keturių egzaminų pirmų dalių rezultatai",
-      imgUrl:
-        "https://s1.15min.lt/images/photos/2014/06/10/original/antradieni-salies-abiturientai-laiko-valstybini-istorijos-brandos-egzamina-5396a617c023d.jpg",
-    },
-    {
-      id: 5,
-      title: "Skelbiami dar keturių egzaminų pirmų dalių rezultatai",
-      imgUrl:
-        "https://s1.15min.lt/images/photos/2014/06/10/original/antradieni-salies-abiturientai-laiko-valstybini-istorijos-brandos-egzamina-5396a617c023d.jpg",
-    },
-  ]);
-
+const Main = ({ setLogoText, characters }) => {
   const [isShowArticle, setShowArticle] = useState(true);
 
   return (
     <main className={styles.main}>
       {isShowArticle && <h1>Articles</h1>}
       <div>
-        {articles.map((a) => {
-          return <Article key={a.id} title={a.title} imgUrl={a.imgUrl} />;
+        {characters.map((c) => {
+          return <Article key={c.id} title={c.name} imgUrl={c.image} />;
         })}
       </div>
       <div className={styles.buttonWrapper}>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,35 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "../components/Header/Header";
 import Main from "../components/Main/Main";
 import Footer from "../components/Fotter/Footer";
-import { useState } from "react";
-import callendarImg from "../assets/callendar.png";
-
+import Spinner from "../components/Spinner/Spinner";
 const MainPage = () => {
   const [logoText, setLogoText] = useState("TYPE 27");
+  const [characters, setCharacters] = useState([]);
 
-  console.log(callendarImg);
+  const fetchCharacters = async () => {
+    const response = await axios.get(
+      "https://hp-api.onrender.com/api/characters"
+    );
+
+    setCharacters(response.data);
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
 
   return (
     <>
       <Header logo={logoText} />
-      <Main setLogoText={setLogoText} />
+      {characters.length > 0 ? (
+        <Main setLogoText={setLogoText} characters={characters} />
+      ) : (
+        <Spinner />
+      )}
+
       <Footer />
-      <img src={callendarImg.src} alt="" />
     </>
   );
 };
